c8edafc487679ee971b15f6ec542eb41c8661081
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-14 22:14:11 +0000

    migration update & use http because windows :(

--- db/migrate/20160314201932_create_contacts.rb
+++ db/migrate/20160314201932_create_contacts.rb
@@ -1,12 +1,13 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      t.string :first
-      t.string :last
       t.string :title
+      t.string :first null: false
+      t.string :last null: false
       t.string :city
       t.string :phone
       t.string :email
+      t.integer :created_by null: false
 
       t.timestamps null: false
     end
