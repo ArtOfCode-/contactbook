e6895c92559ceec097f44d7de1d86eb43f1ca16a
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-14 23:07:40 +0000

    name options right

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
   validates :password, :confirmation => true #password_confirmation attr
-  validates_length_of :password, :min => 6, :on => :create
+  validates_length_of :password, :minimum => 6, :on => :create
 
   attr_accessible :username, :email, :password, :password_confirmation
 
