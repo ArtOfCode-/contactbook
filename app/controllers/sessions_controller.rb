0fc5f91b72168b354952f7160246bdc224024bfd
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-15 16:54:19 +0000

    login attempt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,18 @@
 class SessionsController < ApplicationController
   def login
+    #Login Form
   end
 
-  def home
-  end
-
-  def profile
-  end
-
-  def settings
+  def login_attempt
+    authorized_user = User.authenticate(params[:username_or_email], params[:login_password])
+    if authorized_user
+      flash[:notice] = "You logged in successfully as #{authenticated_user.name}"
+      flash[:color] = "valid"
+      redirect_to(:action => 'home')
+    else
+      flash[:notice] = "Invalid username or password"
+      flash[:color]= "invalid"
+      render "login"
+    end
   end
 end
