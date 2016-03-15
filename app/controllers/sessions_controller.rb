7816e31d3bc6ca1d65624a8bbcd13a2e4c12650f
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-15 16:56:37 +0000

    actually save login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def login_attempt
     authorized_user = User.authenticate(params[:username_or_email], params[:login_password])
     if authorized_user
+      session[:user_id] = authorized_user.id
       flash[:notice] = "You logged in successfully as #{authenticated_user.name}"
       flash[:color] = "valid"
       redirect_to(:action => 'home')
