19a633b38370e7e79c05dd2ce02d448e20654f13
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-15 17:13:57 +0000

    use the right variable

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
   before_filter :save_login_state, :only => [:login, :login_attempt]
 
   def login
-    
+
   end
 
   def login_attempt
     authorized_user = User.authenticate(params[:username_or_email], params[:login_password])
     if authorized_user
       session[:user_id] = authorized_user.id
-      flash[:notice] = "You logged in successfully as #{authenticated_user.name}"
+      flash[:notice] = "You logged in successfully as #{authorized_user.name}"
       flash[:color] = "valid"
       redirect_to(:action => 'home')
     else
