b2e2a1d10745f5a31b23111dfc56e7802abcc693
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-15 16:58:57 +0000

    add before action filters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
+  before_filter :authenticate_user, :only => [:home, :profile, :setting]
+  before_filter :save_login_state, :only => [:login, :login_attempt]
+
   def login
-    #Login Form
+    
   end
 
   def login_attempt
