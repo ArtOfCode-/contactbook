9fc3a4373941b139f1e7ae9ddbc2b5e48455ef05
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-15 16:50:11 +0000

    sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/login'
+
+  get 'sessions/home'
+
+  get 'sessions/profile'
+
+  get 'sessions/settings'
+
   get 'users/sign_up', to: 'users#new'
   post 'users/sign_up', to: 'users#create'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314201932) do
+ActiveRecord::Schema.define(version: 20160314221854) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "first"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20160314201932) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "encrypted_password"
+    t.string   "salt"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
 end
