496d0a193ee221ef485cc907ba4e2a634b023f75
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-15 16:46:04 +0000

    user auth methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
-  validates :password, :confirmation => true #password_confirmation attr
+  validates :password, :confirmation => true
   validates_length_of :password, :minimum => 6, :on => :create
 
   before_save :encrypt_password
@@ -18,5 +18,22 @@ class User < ActiveRecord::Base
 
   def clear_password
     self.password = nil
+  end
+
+  def self.authenticate(username_or_email="", login_password="")
+    if EMAIL_REGEX.match(username_or_email)
+      user = User.find_by_email(username_or_email)
+    else
+      user = User.find_by_username(username_or_email)
+    end
+    if user && user.match_password(login_password)
+      return user
+    else
+      return false
+    end
+  end
+
+  def match_password(login_password="")
+    return encrypted_password == BCrypt::Engine.hash_secret(login_password, salt)
   end
 end
