a94aa7cb9dd72806459d712d0250ff90c9a66157
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-16 16:21:10 +0000

    add exports

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -1,7 +1,13 @@
 class ExportsController < ApplicationController
+  before_filter :authenticate_user
+
   def json
+    contacts = Contact.select("title, first, last, city, phone, email").where("created_by" => @current_user.id)
+    render :json => contacts
   end
 
   def xml
+    contacts = Contact.select("title, first, last, city, phone, email").where("created_by" => @current_user.id)
+    render :xml => contacts
   end
 end
