717bd73199811371988147cb7934f9d938e661b5
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-16 16:46:09 +0000

    add created by to users

--- db/migrate/20160316163620_add_created_by_to_users.rb
+++ db/migrate/20160316163620_add_created_by_to_users.rb
@@ -1,15 +1,5 @@
 class AddCreatedByToUsers < ActiveRecord::Migration
   def change
-    create_table :contacts do |t|
-      t.string :title
-      t.string :first, null: false
-      t.string :last, null: false
-      t.string :city
-      t.string :phone
-      t.string :email
-      t.integer :created_by
-
-      t.timestamps null: false
-    end
+    add_column :contacts, :created_by, :integer, null: false
   end
 end
