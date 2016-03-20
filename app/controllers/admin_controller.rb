77910e320893ca1bd88c8b818f85f88147071014
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-20 15:01:37 +0000

    create admin links view

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,12 @@
 class AdminController < ApplicationController
   def index
   end
+
+  private
+    def verify_admin
+      if @current_user.nil? or !@current_user.is_admin
+        redirect_to root_path
+        return
+      end
+    end
 end
