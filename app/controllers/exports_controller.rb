22cda17feffeff9fdef40b287259e14158859b3a
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-20 19:01:46 +0000

    add id to exports

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -2,12 +2,12 @@ class ExportsController < ApplicationController
   before_filter :authenticate_user
 
   def json
-    contacts = Contact.select("title, first, last, city, phone, email").where("created_by" => @current_user.id)
+    contacts = Contact.select("id, title, first, last, city, phone, email").where("created_by" => @current_user.id)
     render :json => contacts
   end
 
   def xml
-    contacts = Contact.select("title, first, last, city, phone, email").where("created_by" => @current_user.id)
+    contacts = Contact.select("id, title, first, last, city, phone, email").where("created_by" => @current_user.id)
     render :xml => contacts
   end
 end
