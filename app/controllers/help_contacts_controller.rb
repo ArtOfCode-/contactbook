a91620049c39a8c760afbd6afa76ea67a78e701d
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-22 21:42:49 +0000

    updates to help center

--- app/controllers/help_contacts_controller.rb
+++ app/controllers/help_contacts_controller.rb
@@ -1,2 +1,10 @@
 class HelpContactsController < ApplicationController
+  def details
+  end
+
+  def creating
+  end
+
+  def managing
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # ==== HELP CENTER ROUTES === #
   get 'help', to: 'help#index'
 
   get 'help/about', to: 'help#about'
@@ -6,17 +7,25 @@ Rails.application.routes.draw do
   get 'help/about/contact', to: 'help_about#contact'
 
   get 'help/contacts', to: 'help#contacts'
-  # get 'help/contacts/article', to: 'help_contacts#article'
+  get 'help/contacts/details', to: 'help_contacts#details'
+  get 'help/contacts/creating', to: 'help_contacts#creating'
+  get 'help/contacts/managing', to: 'help_contacts#managing'
 
   get 'help/security', to: 'help#security'
   get 'help/security/tech', to: 'help_security#tech'
   get 'help/security/bugs', to: 'help_security#bugs'
 
+  # === ADMIN ROUTES === #
   get 'admin', to: 'admin#index'
+  get 'users', to: 'users#index'
+  get 'contacts/all', to: 'contacts#show_all'
 
+  # === EXPORTS ROUTES === #
   get 'exports/json', to: 'exports#json'
   get 'exports/xml', to: 'exports#xml'
+  get 'exports/vcf/:id', to: 'exports#vcard'
 
+  # === SESSION MANAGEMENT ROUTES === #
   get 'login', to: 'sessions#login'
   post 'login', to: 'sessions#login_attempt'
   get 'logout', to: 'sessions#logout'
@@ -24,18 +33,14 @@ Rails.application.routes.draw do
   get 'sign-up', to: 'users#new'
   post 'sign-up', to: 'users#create'
 
-  get 'users', to: 'users#index'
-
+  # === ACCOUNT MANAGEMENT ROUTES === #
   get 'dashboard', to: 'sessions#home'
   get 'profile', to: 'sessions#profile'
   get 'settings', to: 'sessions#settings'
 
-  get 'exports/json', to: 'exports#json'
-  get 'exports/xml', to: 'exports#xml'
-  get 'exports/vcf/:id', to: 'exports#vcard'
-
-  root to: 'sessions#home'
-
-  get 'contacts/all', to: 'contacts#show_all'
+  # === OBJECT RESOURCES === #
   resources :contacts
+
+  # === SPECIAL ROUTES === #
+  root to: 'sessions#home'
 end
