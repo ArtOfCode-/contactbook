ed67dd990c6edc2a190de5486cc4106f89daaad8
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-22 11:53:48 +0000

    add links to help index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'help_security/tech'
-
-  get 'help_security/bugs'
-
-  get 'help_about/whats_this'
-
-  get 'help_about/contact'
-
   get 'help', to: 'help#index'
+
+  get 'help/about', to: 'help#about'
+  get 'help/about/whats-this', to: 'help_about#whats_this'
+  get 'help/about/contact', to: 'help_about#contact'
+
   get 'help/contacts', to: 'help#contacts'
+  # get 'help/contacts/article', to: 'help_contacts#article'
+
   get 'help/security', to: 'help#security'
+  get 'help/security/tech', to: 'help_security#tech'
+  get 'help/security/bugs', to: 'help_security#bugs'
 
   get 'admin', to: 'admin#index'
 
