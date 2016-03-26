8ac6577369871f2662a37c297ebb7c560dfd1647
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-26 14:32:33 +0000

    add dynamic root

--- app/controllers/dynamic_routes_controller.rb
+++ app/controllers/dynamic_routes_controller.rb
@@ -1,2 +1,11 @@
 class DynamicRoutesController < ApplicationController
+  before_filter :check_login_status, :only => [:root]
+
+  def root
+    if !@current_user.nil?
+      redirect_to contacts_path
+    else
+      redirect_to url_for(:controller => :help_about, :action => :whats_this)
+    end
+  end
 end
