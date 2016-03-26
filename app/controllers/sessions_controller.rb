ae20e77f1b4f29e2c87b745825aef4e0c9d0350a
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-26 20:51:13 +0000

    add DEK to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   before_filter :save_login_state, :only => [:login, :login_attempt]
 
   def login
-
   end
 
   def login_attempt
     authorized_user = User.authenticate(params[:username_or_email], params[:login_password])
     if authorized_user
       session[:user_id] = authorized_user.id
+      session[:dek] = derive_dek
       flash[:notice] = "You logged in successfully as #{authorized_user.username}"
       flash[:color] = "valid"
       redirect_to(:action => 'home')
@@ -22,8 +22,20 @@ class SessionsController < ApplicationController
 
   def logout
     session[:user_id] = nil
+    session[:dek] = nil
     flash[:notice] = "You are now logged out."
     flash[:color] = "valid"
     redirect_to :action => 'login'
   end
+
+  private
+    def encrypt_password(password, salt)
+      BCrypt::Engine.cost = 13
+      return BCrypt::Engine.hash_secret(password, salt)
+    end
+
+    def derive_dek(user)
+      encrypted = encrypt_password(params[:login_password], user.salt)
+      return Digest::SHA2.new(512).hexdigest("#{params[:login_password]}#{encrypted}")
+    end
 end
