c2fe6c0baf01da110a5c11cf4ec9639ea3bc6d48
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-27 16:25:48 +0100

    does that need an @

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
   attr_accessor :encrypted_fields
-  
-  encrypted_fields = ['first', 'last', 'title', 'city', 'phone', 'email']
+
+  @encrypted_fields = ['first', 'last', 'title', 'city', 'phone', 'email']
 end
