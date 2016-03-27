5cb80eb827e0fbfb27ba4639e61d0b36567c8765
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-27 20:20:32 +0100

    correct dashboard route

--- app/controllers/dynamic_routes_controller.rb
+++ app/controllers/dynamic_routes_controller.rb
@@ -3,9 +3,9 @@ class DynamicRoutesController < ApplicationController
 
   def root
     if !@current_user.nil?
-      redirect_to contacts_path
-    else
       redirect_to url_for(:controller => :sessions, :action => :home)
+    else
+      redirect_to url_for(:controller => :help_about, :action => :whats_this)
     end
   end
 end
