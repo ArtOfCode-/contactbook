7278fca3b17a2af05988c881a7c0db52831a0d0a
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-03-28 13:19:45 +0100

    except encrypt from ownership checks

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
   before_filter :authenticate_user
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
   before_action :decrypt_set_contact, only: [:show, :edit]
-  before_action :check_ownership, except: [:index, :new, :create, :show_all]
+  before_action :check_ownership, except: [:index, :new, :create, :show_all, :encrypt]
   before_action :verify_admin, only: [:show_all]
 
   # GET /contacts/all
